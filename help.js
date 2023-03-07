b5a0c67bdb58b4ab652fd074c0b93401f3be6bc3
SenjaTW01 <senjatwilight3@gmail.com>
2023-03-06 16:20:50 -0800

    bot wa

---
+++ help.js
@@ -0,0 +1,19 @@
+
+exports.infoOwner = () =>{
+return`──「 *DATA PROFIL OWNER* 」──
+
+ ⬣ *Nama :* SenjaAnakBaik
+ ⬣ *Umur :* 18
+ ⬣ *Hoby. :* Ngetik Hello World
+ ⬣ *Asal   :* Marsss
+ ⬣ *Status:* Creator
+ ⬣ *Zodiak:* Gk tau
+
+                  「 *SOSIAL MEDIA*」
+ ⬣ *Whatsapp:* 085860266897
+ ⬣ *Youtube:* youtube.com/gada
+ ⬣ *Github:* github.com/SenjaTW01
+ ⬣ *Instagram:* Instagram.com/@senjazxz_
+ ⬣ *website:* zero
+ `
+}
